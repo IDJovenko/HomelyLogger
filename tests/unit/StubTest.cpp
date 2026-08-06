80e8de16d862ccf5b6b11e32ddd040e50dcfc1c8
Dmitriy Jovenko <iovenko.dima30@gmail.com>
2026-08-06 22:43:06 +0700

    chore(tests): remove temporary StubTest file

--- tests/unit/StubTest.cpp
+++
@@ -1,5 +0,0 @@
-#include <gtest/gtest.h>
-
-// Temporary stub to keep CI green during initial development.
-// Remove this file when first real test is added.
-TEST(StubTest, DISABLED_) { EXPECT_TRUE(true); }
